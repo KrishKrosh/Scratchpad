6c842e5ec586d9feb488f153291291e21c6f823b
KrishKrosh <krish.life21@gmail.com>
2026-04-17 02:57:42 -0400

    Initial Commit

---
+++ Scratchpad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scratchpad
+//
+//  Created by Krish Shah on 2026-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scratchpad/ScratchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchpadApp.swift
+//  Scratchpad
+//
+//  Created by Krish Shah on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct ScratchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
